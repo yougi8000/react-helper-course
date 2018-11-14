fdc2be81f159894b86b31a1e61ca00823ed61e5f
maevanapcontact <maevanap.contact@gmail.com>
2018-11-14 19:59:29 +0100

    ProductList component

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import React, { Component } from 'react';
 import './App.css';
 
 import MainMenu from './components/MainMenu';
+import ProductList from './components/ProductList';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <MainMenu name='visitor' />
+        <ProductList />
       </div>
     );
   }
---
+++ src/components/ProductList.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ProductList = props => {
+    return (
+        <section className='product-list'>
+        
+        </section>
+    );
+};
+
+export default ProductList;
