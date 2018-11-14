cc11e7435b1a2ca4094d54d4cba7f3f3d98ca345
maevanapcontact <maevanap.contact@gmail.com>
2018-11-14 21:29:14 +0100

    map through json data

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
 
 import './App.css';
-import * as fetchedBooks from './books.json';
+import fetchedBooks from './books.json';
 
 import MainMenu from './components/MainMenu';
 import ProductList from './components/ProductList';
@@ -13,23 +13,20 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      books: [],
+      books: fetchedBooks,
     };
   }
 
-  componentDidMount() {
-    this.setState({
-      books: fetchedBooks,
-    })
-  }
-
   render() {
+    console.log(this.state.books)
     return (
       <div className="App">
         <MainMenu name='visitor' />
         
         <Route path='/' exact render={() => (
-          <ProductList />
+          <ProductList
+          listBooks={fetchedBooks}
+          />
         )} />
         <Route path='/cart' render={() => (
           <CartList />
--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -5,42 +5,14 @@ import Product from './Product';
 const ProductList = props => {
     return (
         <section className='product-list'>
-            <Product
-                id={1}
-                title='The Psychopath test'
-                genre=' Non-Fiction'
-                image='http://bookcoverarchive.com/wp-content/uploads/2017/01/psychopath-test.jpg'
-            />
-            <Product
-                id={2}
-                title='The Psychopath test'
-                genre=' Non-Fiction'
-                image='http://bookcoverarchive.com/wp-content/uploads/2017/01/psychopath-test.jpg'
-            />
-            <Product
-                id={3}
-                title='The Psychopath test'
-                genre=' Non-Fiction'
-                image='http://bookcoverarchive.com/wp-content/uploads/2017/01/psychopath-test.jpg'
-            />
-            <Product
-                id={4}
-                title='The Psychopath test'
-                genre=' Non-Fiction'
-                image='http://bookcoverarchive.com/wp-content/uploads/2017/01/psychopath-test.jpg'
-            />
-            <Product
-                id={5}
-                title='The Psychopath test'
-                genre=' Non-Fiction'
-                image='http://bookcoverarchive.com/wp-content/uploads/2017/01/psychopath-test.jpg'
-            />
-            <Product
-                id={6}
-                title='The Psychopath test'
-                genre=' Non-Fiction'
-                image='http://bookcoverarchive.com/wp-content/uploads/2017/01/psychopath-test.jpg'
-            />
+            {props.listBooks.map(book => (
+                <Product
+                    id={book.id}
+                    title={book.title}
+                    image={book.image}
+                    genre={book.genre}
+                />
+            ))}
         </section>
     );
 };
