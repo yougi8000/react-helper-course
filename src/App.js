b81be9c563efbf1a87188e671661f61fe34402c9
maevanapcontact <maevanap.contact@gmail.com>
2018-11-14 21:15:34 +0100

    Import JSON books

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
 
 import './App.css';
+import * as fetchedBooks from './books.json';
 
 import MainMenu from './components/MainMenu';
 import ProductList from './components/ProductList';
@@ -9,6 +10,19 @@ import CartList from './components/CartList';
 import ConnexionForm from './components/ConnexionForm';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      books: [],
+    };
+  }
+
+  componentDidMount() {
+    this.setState({
+      books: fetchedBooks,
+    })
+  }
+
   render() {
     return (
       <div className="App">
